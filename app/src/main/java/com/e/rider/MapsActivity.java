ecff7d5b47a6e23dd522b7831a66f89932c4e909
Bhawa prakash <35968702+Bhawa-prakash@users.noreply.github.com>
2019-04-10 19:15:04 +0530

    ggggg

--- app/src/main/java/com/e/rider/MapsActivity.java
+++ app/src/main/java/com/e/rider/MapsActivity.java
@@ -2,6 +2,9 @@ package com.e.rider;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +12,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.callbacks.SubscribeCallback;
+import com.pubnub.api.models.consumer.PNStatus;
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -18,29 +32,63 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        PNConfiguration pnc = new PNConfiguration()
+                .setSubscribeKey("sub-c-cb8aa4e2-55f5-11e9-93f3-8ed1bbcba485");
+        PubNub pubnub = new PubNub(pnc);
+        pubnub.addListener(new SubscribeCallback() {
+            @Override
+            public void status(PubNub pubnub, PNStatus status) {
+
+            }
+
+            @Override
+            public void message(PubNub pubnub, final PNMessageResult message) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        if (!TextUtils.isEmpty(message.getMessage().toString())){
+                            try {
+                                JSONObject jsonObject = new JSONObject(message.getMessage().toString());
+                                double lat = Double.parseDouble(jsonObject.optString("latt"));
+                                double longitute = Double.parseDouble(jsonObject.optString("long"));
+                                 LatLng sydney = new LatLng(lat,longitute);
+                        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                        Toast.makeText(MapsActivity.this, "punbnub data" +
+                                message.getMessage().toString(), Toast.LENGTH_LONG).show();
+
+
+                    }
+                });
+            }
+
+            @Override
+            public void presence(PubNub pubnub, PNPresenceEventResult presence) {
+
+            }
+        });
+        pubnub.subscribe()
+                .channels(Arrays.asList("Rider_channel"))
+                .execute();
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 }
